astiob/Wobbly

commit e7e797f835456c303bb49c14a096eab1c9863282

Make match cycling order more sensible
Fixes Jaded-Encoding-Thaumaturgy/Wobbly#28

src/shared/WobblyProject.cpp
@@ -1957,26 +1957,34 @@ void WobblyProject::setMatch(int frame, char match) {
 }
 
 
-void WobblyProject::cycleMatchBCN(int frame) {
+void WobblyProject::cycleMatchCNB(int frame) {
     if (frame < 0 || frame >= getNumFrames(PostSource))
         throw WobblyException("Can't cycle the match for frame " + std::to_string(frame) + ": frame number out of range.");
 
-    // N -> C -> B. This is the order Yatta uses, so we use it.
+    // C -> N -> B
 
     char match = getMatch(frame);
 
-    if (match == 'n')
-        match = 'c';
-    else if (match == 'c') {
-        if (frame == 0)
-            match = 'n';
-        else
-            match = 'b';
-    } else if (match == 'b') {
-        if (frame == getNumFrames(PostSource) - 1)
-            match = 'c';
-        else
-            match = 'n';
+    while (true) {
+        switch (match) {
+            case 'c':
+                match = 'n';
+                break;
+            case 'n':
+                match = 'b';
+                break;
+            default:
+                match = 'c';
+                break;
+        }
+
+        if (frame == 0 && match == 'b')
+            continue;
+
+        if (frame == getNumFrames(PostSource) - 1 && match == 'n')
+            continue;
+
+        break;
     }
 
     setMatch(frame, match);
@@ -1989,32 +1997,36 @@ void WobblyProject::cycleMatch(int frame) {
     if (frame < 0 || frame >= getNumFrames(PostSource))
         throw WobblyException("Can't cycle the match for frame " + std::to_string(frame) + ": frame number out of range.");
 
-    // U -> B -> N -> C -> P
+    // C -> N -> B -> P -> U
 
     char match = getMatch(frame);
 
-    if (match == 'u') {
-        if (frame == 0)
-            match = 'n';
-        else
-            match = 'b';
-    } else if (match == 'b') {
-        if (frame == getNumFrames(PostSource) - 1)
-            match = 'c';
-        else
-            match = 'n';
-    } else if (match == 'n') {
-        match = 'c';
-    } else if (match == 'c') {
-        if (frame == 0)
-            match = 'u';
-        else
-            match = 'p';
-    } else if (match == 'p') {
-        if (frame == getNumFrames(PostSource) - 1)
-            match = 'b';
-        else
-            match = 'u';
+    while (true) {
+        switch (match) {
+            case 'c':
+                match = 'n';
+                break;
+            case 'n':
+                match = 'b';
+                break;
+            case 'b':
+                match = 'p';
+                break;
+            case 'p':
+                match = 'u';
+                break;
+            default:
+                match = 'c';
+                break;
+        }
+
+        if (frame == 0 && (match == 'b' || match == 'c'))
+            continue;
+
+        if (frame == getNumFrames(PostSource) - 1 && (match == 'n' || match == 'u'))
+            continue;
+
+        break;
     }
 
     setMatch(frame, match);

src/shared/WobblyProject.h
@@ -239,7 +239,7 @@ class WobblyProject : public QObject {
 
         char getMatch(int frame) const;
         void setMatch(int frame, char match);
-        void cycleMatchBCN(int frame);
+        void cycleMatchCNB(int frame);
         void cycleMatch(int frame);
 
 

src/wobbly/WobblyWindow.cpp
@@ -472,7 +472,7 @@ void WobblyWindow::createShortcuts() {
         { "", "Shift+Down",         "Jump to previous combed frame", &WobblyWindow::jumpToPreviousCombedFrame },
         { "", "Alt+Up",             "Jump to next section with pattern failure", &WobblyWindow::jumpToNextPatternFailureSection },
         { "", "Alt+Down",           "Jump to previous section with pattern failure", &WobblyWindow::jumpToPreviousPatternFailureSection },
-        { "", "S",                  "Cycle the current frame's match", &WobblyWindow::cycleMatchBCN },
+        { "", "S",                  "Cycle the current frame's match", &WobblyWindow::cycleMatchCNB },
         { "", "Ctrl+F",             "Replace current frame with next", &WobblyWindow::freezeForward },
         { "", "Shift+F",            "Replace current frame with previous", &WobblyWindow::freezeBackward },
         { "", "F",                  "Freeze current frame or a range", &WobblyWindow::freezeRange },
@@ -5044,11 +5044,11 @@ void WobblyWindow::jumpToPreviousPatternFailureSection() {
 }
 
 
-void WobblyWindow::cycleMatchBCN() {
+void WobblyWindow::cycleMatchCNB() {
     if (!project)
         return;
 
-    project->cycleMatchBCN(current_frame);
+    project->cycleMatchCNB(current_frame);
     commit("Cycle frame's match");
 
     updateSectionOrphanFrames(current_frame);

src/wobbly/WobblyWindow.h
@@ -392,7 +392,7 @@ public slots:
     void deleteFreezeFrame();
     void toggleFreezeFrames();
 
-    void cycleMatchBCN();
+    void cycleMatchCNB();
 
     void toggleDecimation();
 

commit d149169c2263b82537a0be3b5ad5cdce6530af61

cycleMatch: fix 'c'/'p' confusion on video's first frame
It was impossible to set the first frame to 'c', and instead
it was possible to set it to 'p', which doesn't work.

This function doesn't seem to be actually used at all,
but as long as it does exist, it may as well be correct.

Regression in commit e7e797f835456c303bb49c14a096eab1c9863282.

src/shared/WobblyProject.cpp
@@ -1995,7 +1995,7 @@ void WobblyProject::cycleMatch(int frame) {
                 break;
         }
 
-        if (frame == 0 && (match == 'b' || match == 'c'))
+        if (frame == 0 && (match == 'b' || match == 'p'))
             continue;
 
         if (frame == getNumFrames(PostSource) - 1 && (match == 'n' || match == 'u'))