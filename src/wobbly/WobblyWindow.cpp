a51b1d4db516c6ac072ecc0480381b2176bb8757
Oleg Oshmyan <chortos@inbox.lv>
2025-01-19 23:29:49 +0200

    Pattern guessing: add ccccn/kkkkd pattern for 12 fps scenes

--- src/wobbly/WobblyWindow.cpp
+++ src/wobbly/WobblyWindow.cpp
@@ -2186,6 +2186,7 @@ void WobblyWindow::createPatternGuessingWindow() {
     std::map<int, QString> use_patterns = {
         { PatternCCCNN, "CCCNN" },
         { PatternCCNNN, "CCNNN" },
+        { PatternCCCCN, "CCCCN" },
         { PatternCCCCC, "CCCCC" }
     };
     pg_use_patterns_buttons = new QButtonGroup(this);
