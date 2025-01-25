d5050fce98586e9348fd6f89d17c6f475d2eec37
Oleg Oshmyan <chortos@inbox.lv>
2025-01-25 15:53:40 +0200

    Revert "Switch lsmas for bestsource"
    This reverts commit 979d772d748c0ebf6cb894650f47e68cfb0e1770.

--- src/shared/WobblyShared.cpp
+++ src/shared/WobblyShared.cpp
@@ -64,7 +64,7 @@ static PluginDetectionInfo requiredPlugins[] = {
     {"FieldHint", "com.nodame.fieldhint", "FieldHint", nullptr},
     {"TDeintMod", "com.holywu.tdeintmod", "IsCombed", nullptr},
     {"d2vsource", "com.sources.d2vsource", "Source", nullptr},
-    {"BestSource", "com.vapoursynth.bestsource", "VideoSource", nullptr},
+    {"L-SMASH-Works", "systems.innocent.lsmas", "LibavSMASHSource", "LWLibavSource"},
     {"DGDecNV", "com.vapoursynth.dgdecodenv", "DGSource", nullptr}
 };
 
