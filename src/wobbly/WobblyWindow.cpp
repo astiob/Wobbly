70987f2c7e760d1f11538d1abf6bef9824c3bc99
Oleg Oshmyan <chortos@inbox.lv>
2025-11-22 21:24:52 +0200

    Fix interlaced fade list always missing the last fade
    This should've been prefix decrement, not postfix.
    But there's no reason to perform this check within the loop at all.

--- src/wobbly/WobblyWindow.cpp
+++ src/wobbly/WobblyWindow.cpp
@@ -3722,9 +3722,8 @@ void WobblyWindow::updateFadesWindow() {
         } else {
             end = it->first;
         }
-        if (it == (fades.cend()--))
-            fades_ranges.push_back({ start, end });
     }
+    fades_ranges.push_back({ start, end });
 
     fades_table->setRowCount(fades_ranges.size());
 
